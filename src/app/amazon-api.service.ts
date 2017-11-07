302c07b3c5001cc7dddeefd8b74f06e7e2e114d8
David Hale and Sarah Lindmar <david@>
2017-11-07 08:21:45 -0800

    import http

--- src/app/amazon-api.service.ts
+++ src/app/amazon-api.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { Http } from '@angular/http'
 
 @Injectable()
 export class AmazonApiService {
 
   constructor(private http: Http) {
-    
+
   }
 
 }
