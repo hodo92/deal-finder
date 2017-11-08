fa22ff06948f5f5fbf56828395cf6dcaa7330cfc
Guest User <Guest@Epicodus-6B.local>
2017-11-08 15:19:53 -0800

    auth working...finally

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from '../authentication.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,11 +9,28 @@ import { AuthenticationService } from '../authentication.service';
   providers: [AuthenticationService]
 })
 export class HeaderComponent {
+  user;
+  private isLoggedIn: Boolean;
+  private userName: String;
 
-  constructor(public authService: AuthenticationService) { }
+  constructor(public authService: AuthenticationService, private router: Router) {
+    this.authService.user.subscribe(user => {
+      if (user == null) {
+        this.isLoggedIn = false;
+        this.router.navigate(['public']);
+      } else {
+        this.isLoggedIn = true;
+        this.userName = user.displayName;
+        this.router.navigate([]);
+      }
+    });
 
-  authLogin() {
+  }
+  login() {
     this.authService.login();
   }
 
+  logout() {
+    this.authService.logout();
+  }
 }
